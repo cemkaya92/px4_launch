af0f0c237501e2d4a583c30d231b34cd5b719ee1
Uluhan Cem Kaya <uluhancem.kaya@uta.edu>
2025-07-11 18:08:42 -0500

    ros_gz_bridge camera topics are transported from gz-harmonic to ROS2

--- launch/f450_rover_collaboration.launch.py
+++ launch/f450_rover_collaboration.launch.py
@@ -22,5 +22,21 @@ def generate_launch_description():
             executable='f450_rover_collaboration',
             name='f450_rover_collaboration',
             prefix='gnome-terminal --'
+        ),
+
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='gz_camera_bridge',
+            output='screen',
+            arguments=[
+                # Bridge the image topic
+                '/world/default/model/f450_mono_cam_0/link/camera_link/sensor/imager/image'
+                '@sensor_msgs/msg/Image@gz.msgs.Image',
+                
+                # Bridge the camera_info topic
+                '/world/default/model/f450_mono_cam_0/link/camera_link/sensor/imager/camera_info'
+                '@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo'
+            ]
         )
     ])
